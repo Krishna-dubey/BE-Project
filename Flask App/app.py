0c55036f0f152cfb00859e79b375d9ce084b9c9a
Krishna-dubey <krishnadubey1997@gmail.com>
2020-06-15 20:03:00 +0530

    login page created

--- Flask App/app.py
+++ Flask App/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
 
 @app.route('/')
 def landing():
-    return "This is Landing Page"
+    return render_template("login.html")
 
 app.run(debug=True)
